e997abf06715fd44b8a75869282aa2e7ce67f21a
Alexandr <cast-tex@yandex.ru>
2021-02-01 15:58:07 +0300

    start project

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="files/css/style.css">
+    <link rel="javascript" href="files/js/scripts.js">
+    <title><h1><?=$title_txt?></h1></title>
+  </head>
+  <body>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include_once "header.php";
+?>
+
+тут пока ничего нет
+
+<?
+include_once "footer.php";
+?>
