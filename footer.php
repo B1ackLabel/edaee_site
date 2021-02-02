6b0e6ac7594652259d3ae1985f5cd411b15a1175
Alexandr <cast-tex@yandex.ru>
2021-02-02 03:34:36 +0300

    ready-made home page of the site

--- footer.php
+++ footer.php
@@ -1,2 +1,19 @@
+</div>
+
+<div class="block-2">
+  <div class="row">
+    название блока
+    <hr />
+    содержимое блока
+  </div>
+</div>
+
+</div>
+
+<footer class="navbar">
+  Все права принадлежат кому то и когда то
+  <a href="#">Вверх</a>
+</footer>
+</center>
 </body>
 </html>
--- header.php
+++ header.php
@@ -2,8 +2,21 @@
 <html lang="ru" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="discription" content="Рецепты для всех моментов в жизни.">
     <link rel="stylesheet" href="files/css/style.css">
     <link rel="javascript" href="files/js/scripts.js">
-    <title><h1><?=$title_txt?></h1></title>
+  <h1><title><?=$title_txt?></title></h1>
   </head>
   <body>
+    <center>
+    <header>
+      <img class="logo" src="files/img/logo.jpg" alt="" />
+    </header>
+    <div class="container tleft">
+      <menu class="navbar" type="toolbar">
+          <a href="/">Ссылка 1</a>
+          <a href="/">Ссылка 2</a>
+          <a href="/">Ссылка 3</a>
+          <a href="/">Ссылка 4</a>
+      </menu>
+      <div class="block-1">
